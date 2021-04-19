21cca53d9a13fd4ac2bcd35cb8a3d168c9f261a4
Victor <victorcs333@gmail.com>
2021-04-19 18:40:44 -0500

    variablesGO

---
+++ ejerc02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import {
+	"fmt"
+}
+
+var numero int
+var texto string
+var status bool
+
+//float32 75334395
+func main() {
+	fmt.Println("Hola Mundo")
+	var numero2 int
+
+	//solo se hace una vez
+	numero3 := 4
+	fmt.Println(numero2)
+	fmt.Println(numero3)
+}

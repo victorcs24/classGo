d408a6e07856619ba540810f7c7a9f1db329ea63
Victor <victorcs333@gmail.com>
2021-04-20 18:16:17 -0500

    funciones

--- ejerc02/main.go
+++ ejerc02/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //var numero int
 //var texto string
@@ -12,6 +10,47 @@ import (
 var numero1, numero2 int
 
 func main() {
+	fmt.Printf("numero %d", doblenum(4))
+
+	numx, result := dobleparam(3)
+	fmt.Println("\n Doble parametro")
+	fmt.Println(numx)
+	fmt.Println(result)
+	fmt.Println("\n Metodo calcular")
+	calcular(2, 4, 6, 7)
+	fmt.Println("\n Metodo calcular")
+	calcular(7, 4, 5)
+
+}
+
+func doblenum(numero int) (z int) {
+	z = numero * 2
+	return z
+}
+
+func dobleparam(numero int) (int, bool) {
+	if numero == 2 {
+		return (numero * 2), true
+	} else {
+		return (numero * 3), false
+	}
+}
+
+func calcular(numero ...int) int {
+	total := 0
+
+	//for _, num := range numero {
+	for item, num := range numero {
+		total = total + num
+		fmt.Printf("\n item: %d total: %d", item, total)
+	}
+	return total
+}
+
+func calcular2(numero ...int) int {
+}
+
+func main_X2() {
 	//fmt.Println("Hola Mundo")
 	//var numero2 int
 	//solo se hace una vez
@@ -27,5 +66,4 @@ func main() {
 	} else {
 		fmt.Printf("Suma num1 menor [%d],[%d]", numero1, numero2)
 	}
-
 }
